bbb65fae8094f0fa444c47e719b176b9489eae18
Prashanth Xavier <prashanth.chinnappa@asml.com>
2024-12-31 16:17:34 +0100

    updated

--- src/items/simple_cli.rs
+++ src/items/simple_cli.rs
@@ -21,12 +21,3 @@ fn main() -> Result<()> {
    }
    Ok(())
 }
-// use std::{thread, time::Duration};
-// fn main() {
-//     let bar = indicatif::ProgressBar::new(1000);
-//     for _i in 0..1001{
-//         thread::sleep(Duration::from_millis(10));
-//         bar.inc(1);
-//     }
-//     bar.finish()
-// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+fn answer()-> i32 {
+    let x: i32 = 42;
+    x
+}
+
 fn main() {
-    println!("PlayGround");
+    let val = answer();
+    println!("{}",val)
+}
+
+#[test]
+fn check_validity() {
+    assert_eq!(answer(), 42)
 }
