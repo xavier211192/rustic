e491bd1fe0ffcd2cc4e14fad58f259c2274a02e2
Prashanth Xavier <xavier211192@gmail.com>
2025-02-20 17:11:04 +0100

    Feature/work (#4)
    * Struct practice
    
    * Struct practice
    
    * Struct practice
    
    ---------
    
    Co-authored-by: Prashanth Xavier <prashanth.chinnappa@asml.com>

---
+++ src/items/struct_rectangle.rs
@@ -0,0 +1,20 @@
+//derive trait to allow displaying struct
+#[derive(Debug)]
+struct Rectangle {
+    width:u32,
+    height:u32,
+}
+
+
+fn main() {
+    let rect1 = Rectangle{
+        width:12,
+        height:12,
+    };
+    println!("{:#?}",rect1); //{:#7} to make it more readable
+    println!("Area is {}",area(&rect1));
+}
+
+fn area(rec:&Rectangle) -> u32 {
+    rec.width * rec.height
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
+//derive trait to allow displaying struct
+#[derive(Debug)]
+struct Rectangle {
+    width:u32,
+    height:u32,
+}
+//to create method start impl everything in this block will be associated with the Rectangle type(struct)
+//these methods are called associated functions
+impl Rectangle{
+    //method takes arguments and first argument is self which is instance of struct
+    fn area(self:&Self)->u32{
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other:&Rectangle)->bool {
+        self.width > other.width && self.height > other.height
+    }
+
+}
+
 fn main() {
-    let height = 12;
-    let width = 12;
-
-    println!("Area is {}",area(width,height));
-}
-
-fn area(width:u32,height:u32) -> u32 {
-    width*height
-}
+    let rect1 = Rectangle{
+        width:12,
+        height:12,
+    };
+    let rect2 = Rectangle{
+        width:5,
+        height:5,
+    };
+    println!("{:#?}",rect1); //{:#7} to make it more readable
+    println!("Area is {}",rect1.area());
+    println!("Can rec1 hold rect 2? {}",rect1.can_hold(&rect2));
