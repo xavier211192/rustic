d3451f6d9760dc549f28b11ca8488f77f8164720
Prashanth Xavier <xavier211192@gmail.com>
2025-03-05 14:34:00 +0100

    Feature/work (#7)
    * Struct practice
    
    * Struct practice
    
    * Struct practice
    
    * struct
    
    * fix typo
    
    * enum basics
    
    ---------
    
    Co-authored-by: Prashanth Xavier <prashanth.chinnappa@asml.com>

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,48 @@
-//derive trait to allow displaying struct
-#[derive(Debug)]
-struct Rectangle {
-    width:u32,
-    height:u32,
+// Enums give a way to say that a value is one of the possible set of values.
+// enumerate all possible variants.
+
+enum IpAddr {
+    V4(u8,u8,u8,u8),
+    V6(String)
 }
-//to create method start impl everything in this block will be associated with the Rectangle type(struct)
-//these methods are called associated functions
-impl Rectangle{
-    //method takes arguments and first argument is self which is instance of struct
-    fn area(self:&Self)->u32{
-        self.width * self.height
+//data can be put directly in each enum variant
+//each variant can have different types and amounts of associated data(not possible in struct)
+//any kind of data can be put inside enum variant including a struct and enum itself.
+let home = IpAddr::V4(127,0,0,1);
+let home2 = IpAddr::V6(String::from("::1"));
+
+//complex enum
+enum Message {
+    Quit,
+    Write(String),
+    Move{x:i32, y:i32},
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message{
+    fn call(&self){
+        //do something
     }
-
-    fn can_hold(&self, other:&Rectangle)->bool {
-        self.width > other.width && self.height > other.height
-    }
-
 }
 
-fn main() {
-    let rect1 = Rectangle{
-        width:12,
-        height:12,
-    };
-    let rect2 = Rectangle{
-        width:5,
-        height:5,
-    };
-    println!("{:#?}",rect1); //{:#7} to make it more readable
-    println!("Area is {}",rect1.area());
-    println!("Can rec1 hold rect 2? {}",rect1.can_hold(&rect2));
-}
+let m = Message::Write(String::from("hey"));
+m.call();
+
+//Option enum type(std library)
+//Rust does not have NULL feature
+//Option and its variants are already in scope in prelude.
+//  enum Option<T> {
+//      None,
+//      Some(T),
+//      }
+let some_number = Some(1); //variant of Option type
+let some_char = Some("h");
+let absent_number: Option<i32> = None; //variant of Option type
+
+//Why is it important
+// compiler will not compile this code because Option<T> is a different type.
+let x:i8 =5;
+let y:Option<i8>= Some(5);
+let sum = x+y;
+//if a value can possibly be null then opt in using Option<T> so you are forced to handle that case.
+//in order to use Option<T> value you should have code that handles all variants of Option.
